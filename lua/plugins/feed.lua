f58ca073ca129e43d581b124d144bb39c8580ea7
Espen Ødegaard <esod@esod.no>
2026-02-24 13:22:30 +0100

    feed.lua template testing

---
+++ lua/plugins/feed.lua
@@ -0,0 +1,14 @@
+return {
+  -- "neo451/feed.nvim",
+  -- cmd = "Feed",
+  -- ---@module 'feed'
+  -- ---@type feed.config
+  -- opts = {
+  --   feeds = {
+  --     -- "github://neo451/feed.nvim",
+  --     "https://neovim.io/news.xml",
+  --     "https://9to5linux.com/feed",
+  --     "https://neilzone.co.uk/index.xml",
+  --   },
+  -- },
+}
