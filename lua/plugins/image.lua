dee916f3f82490472d2b0075ee62beb15f894c29
Espen Ødegaard <esod@esod.no>
2026-02-24 13:21:52 +0100

    add snacks.nvim image support

---
+++ lua/plugins/image.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    image = {
+      -- force = true,
+      -- your image configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}
